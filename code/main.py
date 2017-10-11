c33bc006119ccfc0627f4004ee0062f78f65fb64
kevinhowbrook <kbhowbrook@gmail.com>
2017-10-11 14:10:26 +0100

    screen shots

--- code/main.py
+++ code/main.py
@@ -3,14 +3,14 @@
 from pyvirtualdisplay import Display
 from selenium import webdriver
 
-display = Display(visible=0, size=(800, 600))
+display = Display(visible=0, size=(1200, 800))
 display.start()
 
 # now Firefox will run in a virtual display.
 # you will not see the browser.
 browser = webdriver.Firefox()
-browser.get('http://google.com')
-print browser.title
+browser.get('http://google.com/')
+print(browser.title)
+browser.save_screenshot('/code/ss.png')
 browser.quit()
-
 display.stop()
