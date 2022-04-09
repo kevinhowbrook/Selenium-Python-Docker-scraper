f2222f3565d06a1ea0342a9710ba58ae1fe76ed9
Kevin <2128707+kevinhowbrook@users.noreply.github.com>
2022-04-09 08:36:46 +0100

    Update example.py

--- code/example.py
+++ code/example.py
@@ -16,7 +16,7 @@ class Example:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--window-size=1200x800")
         chrome_options.add_argument('--disable-dev-shm-usage')
-        browser = webdriver.Chrome(chrome_options=chrome_options, executable_path='/usr/bin/chromedriver')
+        browser = webdriver.Chrome(options=chrome_options)
         browser.get('http://google.com/')
 
         ## test bs4
